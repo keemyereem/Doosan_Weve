e2035d2a988e26dcbc79a21cc20851113db53d4a
publisher4m <whm1001@4-m.co.kr>
2022-12-23 18:31:01 +0900

    공사단지 카카오맵 스크립트 병합_2022.12.23

--- js/common.js
+++ js/common.js
@@ -322,6 +322,64 @@ var commonEvent = {
         } else {
           openProcessor();
         }
+
+        // var searchKey = $(this).data('addr');
+        // var markerTit = $(this).data('tit');
+        // 페이지 초기화
+        $('#kakaoMap').html('loading...');
+        $('[data-handler="btn-map-detail"]').attr('href','javascript:alert("잠시만 기다려주세요.")');
+        
+         $.ajax({
+         type : "get",
+         url:'https://dapi.kakao.com/v2/local/search/address.json?query='+encodeURIComponent(locate),
+         headers: {'Authorization' : 'KakaoAK e907ed20e50441767ce164877793fe6d'},
+         success : function(data) {
+           if( data !=null ){
+             if( data.meta.total_count > 0 ){
+               addr_y = data.documents[0].y;
+               addr_x = data.documents[0].x;
+               
+               // randering map
+                 var mapContainer = document.getElementById('kakaoMap'); // 지도를 표시할 div 
+                     var coords = new kakao.maps.LatLng(addr_y, addr_x);
+                   mapOption = {
+                       center: coords,
+                       level: 3
+                   };
+                   
+                 // 지도를 생성합니다    
+                var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords
+                });
+
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                  /* 221222: 기획팀 요청으로 장소설명 삭제
+                var infowindow = new kakao.maps.InfoWindow({
+                    content: '<div style="min-width:150px;text-align:center;padding:6px 2px;">'+markerTit+'</div>'
+                });
+                infowindow.open(map, marker);
+                */
+
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                map.setCenter(coords);
+              $('[data-handler="btn-map-detail"]').attr('href','https://map.kakao.com/?map_type=TYPE_MAP&q='+ encodeURIComponent(locate) + '&urlLevel=2');
+             } else {
+               console.log(locate +"..............no data...............");	
+             }
+           } else {
+            // mapContainer.html();
+             console.log("..............no data...............");
+           }
+           
+         },
+         error : function(error) {
+           console.log('kakao map',error.responseText);
+         }
+       });
     });
 
     popupUI.children("a").hover(
@@ -336,9 +394,9 @@ var commonEvent = {
     );
 
     //공통 팝업코드
-    $(".openPopup").on("click", () => {
-      openProcessor();
-    });
+    // $(".openPopup").on("click", () => {
+    //   openProcessor();
+    // });
     
     // ajax 페이지에서도 이벤트가 적용되도록 작업. 위에 339~341과 동일한 역할을 합니다. 위에 코드 삭제 여부는 검토 부탁드려요
     $(document).on("click",".openPopup", () => {

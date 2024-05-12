1c319478fdc76ca52436fabfc2356f0266ceb6d0
mhk-4m <mhk@4-m.co.kr>
2024-05-12 17:07:29 +0900

    [weve-priviledge-mhk] header, footer 작업 완료

--- js/privilege.js
+++ js/privilege.js
@@ -1,12 +1,260 @@
+/*
+https://agal.tistory.com/m/210
+
+- 참고 소스
+https://codepen.io/BrianCross/pen/PoWapLP
+https://codepen.io/GreenSock/pen/ExEOeJQ
+https://codepen.io/cassie-codes/pen/vYWvwXV
+
+http://everylastdrop.co.uk/
+https://lpla.tistory.com/107
+*/
 var privEvent = {
   init: function () {
-    this.scrollMotion();
+    this.setScrollMotion();
 
     if ($(window).width() <= 768) {
       this.privMSwiper();
     }
   },
+  setScrollMotion: () => {
+    let sectionList = gsap.utils.toArray('[gsap-motion-scene]');
+    const markerConf = {
+      timeline: {
+        startColor: 'black',
+        endColor: 'black',
+        fontWeight: 'bold',
+        indent: 200,
+      },
+      scroll: {
+        startColor: 'red',
+        endColor: 'red',
+        fontWeight: 'bold',
+        indent: 0,
+      },
+    };
+    /* >>>>>  set time line : header */
+    const getSceneTimeLineHeader = () => {
+      const tl = gsap.timeline({
+        markers: markerConf.timeline,
+        paused: true, //정지된 상태로 변수 생성
+        onUpdate: (e) => {
+          if (tl._time > 5) {
+            $('.sec0-list').addClass('open');
+          } else {
+            $('.sec0-list').removeClass('open');
+          }
+        },
+        onStart: () => {
+          console.log('@@@ onStart', tl);
+        },
+        onComplete: () => {
+          console.log('@@@ onComplete', tl);
+        },
+      });
+      return tl
+        .to('.sec0-tit01', { y: -40, duration: 1.5 })
+        .to('.sec0-tit02', { y: -10, opacity: '1', duration: 1.5 })
+        .to('.sec0-tit01, .sec0-tit02', {
+          transform: 'scale(0.8)',
+          duration: 1,
+        })
+        .to('.sec0-list01', { opacity: '1', duration: 1 }, 8)
+        .to('.sec0-list02', { opacity: '1', duration: 1 })
+        .to('.sec0-list03', { opacity: '1', duration: 1 })
+        .to('.sec0-list04', { opacity: '1', duration: 1 })
+        .to('.sec0-list05', { opacity: '1', duration: 1 });
+    };
 
+    const getSceneTimeLineBody = () => {
+      var // $panels = sections[i],
+        $panels = document.querySelector('.panel.active'),
+        $panelCon = $panels.querySelector('.panel-con'),
+        $anchors = $panels.querySelector('.anchor-nav'),
+        $tag = $panels.querySelector('.con_tag'),
+        $tit1 = $panels.querySelector('.con_tit01'),
+        $tit2 = $panels.querySelector('.con_tit02'),
+        $txt = $panels.querySelector('.con_txt'),
+        $conList = $panels.querySelector('.con_list'),
+        $conListBox = $panels.querySelectorAll('.con_list li'),
+        $page = $panels.querySelector('.swiper-pagination'),
+        $nav = document.querySelector('.anchor-nav'),
+        dataColor = $panels.getAttribute('data-color'),
+        dataBg = $panels.getAttribute('data-bg'),
+        panelPadding = '120px 200px';
+      if ($(window).width() <= 768) {
+        panelPadding = '80px 20px';
+      } else if ($(window).width() <= 1500 && $(window).width() > 768) {
+        panelPadding = '120px 100px';
+      } else {
+        panelPadding = '120px 200px';
+      }
+      const tl = gsap.timeline({
+        markers: markerConf.timeline,
+        paused: true, //정지된 상태로 변수 생성
+        onUpdate: (e) => {
+          if (tl._time > 5) {
+            $('.sec0-list').addClass('open');
+          } else {
+            $('.sec0-list').removeClass('open');
+          }
+        },
+        onStart: () => {
+          console.log('@@@ onStart', tl);
+        },
+        onComplete: () => {
+          console.log('@@@ onComplete', tl);
+        },
+      });
+      return tl.from($panels, { height: '851px' }).from($panelCon, {
+        width: 'calc(100% - 34px)',
+        height: '688px',
+        borderRadius: '20px',
+      });
+
+      tl2_1
+        .to('.panel_wrap', { backgroundColor: dataBg }, '<')
+        .to($tit1, { opacity: 0, duration: 0.3, delay: 1 }, '<')
+        .to($txt, { opacity: 0, duration: 0.3, delay: 1 }, '<')
+        .to($tit1, { display: 'none', duration: 0, delay: 1 }, '<')
+        .to($txt, { display: 'none', duration: 0, delay: 1 }, '<')
+        .to($panelCon, { background: dataColor, duration: 0 }, '<')
+        .to(
+          $panelCon,
+          {
+            width: '100%',
+            height: '100%',
+            borderRadius: '0',
+            bottom: 0,
+            padding: panelPadding,
+            duration: 2,
+            delay: 1,
+          },
+          '+=5'
+        )
+        .to(
+          $panels,
+          {
+            width: '100%',
+            height: '100%',
+            /* padding: '0', */ duration: 2,
+            delay: 1,
+          },
+          '<'
+        )
+        // .to($nav, { opacity: 0, duration: 0,},'<')
+
+        .to($tag, { display: 'inline-flex', duration: 0 }, '+=5')
+        .to($tit2, { display: 'flex', duration: 0 }, '<')
+        .to($conList, { display: 'block', opacity: 1 }, '<')
+        .to($tag, { opacity: 1, duration: 0.3 }, '+=5')
+        .to($tit2, { opacity: 1, duration: 0.3 }, '+=0.5')
+        .to($conListBox, { opacity: 1, stagger: 0.5 }, '+=10')
+        .to($panelCon, { backgroundColor: dataColor, duration: 5 }, '<')
+        .to($page, { opacity: 1 }, '+=5')
+        .to($panelCon, { backgroundColor: dataColor, duration: 5 }, '+=5')
+        .reverse();
+    };
+
+    const getSceneTimeLineBottom = () => {
+      let txt2MaxW = 5000,
+        txt2MaxH = 5000,
+        txt1H = 140;
+
+      if ($('#pc').length) {
+        (txt1H = 140), (txt2MaxW = 0), (txt2MaxH = 5000);
+      } else if ($('#mobile').length) {
+        (txt1H = 85), (txt2MaxW = 5000), (txt2MaxH = 0);
+      }
+      const tl = gsap.timeline({
+        markers: markerConf.timeline,
+        onUpdate: (e) => {
+          // onUpdate
+        },
+        onStart: () => {
+          console.log('@@@ onStart', tl);
+        },
+        onComplete: () => {
+          console.log('@@@ onComplete', tl);
+        },
+      });
+      return tl
+        .to('.gsap3', { opacity: 1, y: 0, duration: 0.3 })
+        .to('.gsap3-2', {
+          maxWidth: txt2MaxW,
+          maxHeight: txt2MaxH,
+          duration: 0.6,
+        })
+        .to('.gsap3-1', { height: txt1H, duration: 0.2 })
+        .to('.gsap3-1', { opacity: 0, duration: 0.2 })
+        .to('.gsap3-3', { opacity: 1, duration: 0.2 })
+        .to('.gsap3', { zIndex: 1, delay: 0.1 });
+    };
+
+    const getTimeline = (sectionId) => {
+      switch (sectionId) {
+        case 'header':
+          return getSceneTimeLineHeader();
+        case 'body':
+          return getSceneTimeLineBody();
+        case 'bottom':
+          return getSceneTimeLineBottom();
+      }
+      return null;
+    };
+
+    /* >>>>>  set  scroll event : gsap-motion-scene */
+    gsap.registerPlugin(ScrollTrigger);
+    sectionList.forEach((section, i) => {
+      const sectionId = section.getAttribute('gsap-motion-scene');
+      const tl = getTimeline(sectionId);
+      console.log('!!! init', sectionId, tl);
+      ScrollTrigger.create({
+        markers: markerConf.scroll,
+        trigger: section,
+        //        start: 'top top',
+        end: (self) => {
+          const scrub = self.vars.scrub;
+          if (!scrub) {
+            // 자동 애니메이션일 경우
+            // TODO : 애니메이션이 끝나면 다음 장면으로 넘어가도록 스크립트 추가 필요
+            return `+=${tlSceneHeader._end * 2000}`;
+          } else {
+            // 스크롤 애니메이션일 경우
+            // TODO : 속도는 배율로 조정하세요
+            return `+=${section.offsetHeight * 5}`;
+          }
+        },
+        scrub: true, // 스크롤로 애니메이션을 제어
+        pin: true,
+        pinSpacing: true,
+        animation: tl,
+        onToggle: (self) => console.log('toggled, isActive:', self.isActive),
+        onUpdate: (self) => {
+          console.log(
+            'progress:',
+            self.progress.toFixed(3),
+            'direction:',
+            self.direction,
+            'velocity',
+            self.getVelocity()
+          );
+        },
+        onEnter: (self) => {
+          console.log('### onEnter');
+          //tl.play();
+        },
+        onEnterBack: (self) => {
+          console.log('### onEnterBack');
+          // 아래에서 위로 진입시 역방향 재생을 원할 경우
+          //tl.reverse();
+          // 아래에서 위로 진입시 정방향 재생을 원할 경우
+          //tl.restart(true, false);
+        },
+      });
+    });
+  },
+  setTest: () => {},
   privMSwiper: () => {
     var $sections = document.querySelectorAll('.privilege .section');
     $sections.forEach((item, index) => {
@@ -27,193 +275,5 @@ var privEvent = {
         },
       });
     });
-  },
-
-  scrollMotion: () => {
-    // 1400px 이하 가로스크롤 이동 시 헤더 위치 변경(fixed 속성 대안)
-    $(window).on('scroll', function () {
-      $(
-        '.privilege .section00, .privilege .section, .privilege .section06'
-      ).css('left', 0 - $(this).scrollLeft());
-    });
-
-    var tl1 = gsap.timeline({
-      scrollTrigger: {
-        // markers: true,
-        trigger: '.section00',
-        pin: '.section00',
-        pinSpacing: true,
-        scrub: true,
-        start: 'top top',
-        end: '+=100%',
-        // end: () => `+=${document.querySelector('.section00').offsetHeight}`,
-        onUpdate: function (scrollTrigger) {
-          var progress = scrollTrigger.progress;
-
-          // 스크롤 진행률이 특정 값 이상인 경우 클래스 토글
-          if (progress > 0.4) {
-            $('.sec0-list').addClass('open');
-          } else {
-            $('.sec0-list').removeClass('open');
-          }
-        },
-      },
-    });
-
-    tl1
-      .to('.sec0-tit01', { y: -40, duration: 3 })
-      .to('.sec0-tit02', { y: -10, opacity: '1', duration: 3 })
-      .to('.sec0-tit01, .sec0-tit02', {
-        transform: 'scale(0.8)',
-        /* fontSize: '60px', */ duration: 2,
-      })
-      .to('.sec0-list01', { opacity: '1', duration: 2 }, 16)
-      .to('.sec0-list02', { opacity: '1', duration: 2 })
-      .to('.sec0-list03', { opacity: '1', duration: 2 })
-      .to('.sec0-list04', { opacity: '1', duration: 2 })
-      .to('.sec0-list05', { opacity: '1', duration: 12 });
-
-    var $sections = document.querySelectorAll('.section');
-    $sections.forEach((item, index) => {
-      tl2 = gsap.timeline({
-        scrollTrigger: {
-          // markers: true,
-          trigger: item,
-          // pin: true,
-          pinSpacing: false,
-          scrub: 3,
-          start: 'top bottom',
-          end: '20% 100%',
-          ease: 'none',
-        },
-      });
-
-      tl2_1 = gsap.timeline({
-        scrollTrigger: {
-          // markers: {
-          // startColor: "blue",
-          // endColor: "yellow"
-          // },
-          trigger: item,
-          pin: true,
-          pinSpacing: false,
-          scrub: 1,
-          start: 'top 100%',
-          end: 'bottom 70%',
-          ease: 'none',
-        },
-      });
-
-      var $panels = item.querySelectorAll('.panel'),
-        $panelCon = item.querySelector('.panel-con'),
-        $anchors = item.querySelector('.anchor-nav'),
-        $tag = item.querySelector('.con_tag'),
-        $tit1 = item.querySelector('.con_tit01'),
-        $tit2 = item.querySelector('.con_tit02'),
-        $txt = item.querySelector('.con_txt'),
-        $conList = item.querySelector('.con_list'),
-        $conListBox = item.querySelectorAll('.con_list li'),
-        $page = item.querySelector('.swiper-pagination'),
-        $nav = item.querySelector('.anchor-nav'),
-        dataColor = item.getAttribute('data-color'),
-        panelPadding = '120px 200px';
-
-      if ($(window).width() <= 768) {
-        panelPadding = '80px 20px';
-      } else if ($(window).width() <= 1500 && $(window).width() > 768) {
-        panelPadding = '120px 100px';
-      } else {
-        panelPadding = '120px 200px';
-      }
-
-      tl2
-        .to($sections[index], { yPercent: -100, duration: 0.4 }, 0)
-        .to($panels, { y: 0, duration: 0.6, delay: 0.2 }, 0);
-
-      tl2_1
-        .to($tit1, { opacity: 0, duration: 0.2, delay: 1 }, 4)
-        .to($txt, { opacity: 0, duration: 0.2, delay: 1 }, 4)
-        .to($tit1, { display: 'none', duration: 0, delay: 1 }, 4)
-        .to($txt, { display: 'none', duration: 0, delay: 1 }, 4)
-        .to($panelCon, { zIndex: 1 }, 4)
-        .to($panelCon, { background: dataColor, duration: 0 }, 5)
-        .to($panelCon, { height: '100%', duration: 1 }, 6)
-        .to($panels, { width: '100%', height: '100%', duration: 1 }, 6)
-        .to($sections[index], { padding: '0', duration: 1 }, 6)
-        .to(
-          $panelCon,
-          {
-            width: '100%',
-            height: '100%',
-            borderRadius: '0',
-            bottom: 0,
-            padding: panelPadding,
-            duration: 1.2,
-          },
-          6
-        )
-        .to($nav, { opacity: 0, duration: 0 }, 6)
-
-        .to($tag, { display: 'inline-flex', duration: 0 }, 7)
-        .to($tit2, { display: 'flex', duration: 0 }, 7)
-        .to($conList, { display: 'block', opacity: 1 }, 7)
-        .to($tag, { opacity: 1, duration: 0.2 }, 8)
-        .to($tit2, { opacity: 1, duration: 0.2, delay: 0.3 }, 8)
-        .to($conListBox, { opacity: 1, stagger: 0.2, delay: 0.6 }, 8)
-        .to($panelCon, { backgroundColor: dataColor, duration: 5 }, 8)
-        .to($page, { opacity: 1 }, 10)
-        .to($panelCon, { backgroundColor: dataColor, duration: 5 }, 12);
-    });
-
-    gsap.registerPlugin(ScrollToPlugin);
-
-    /* Main navigation */
-    document.querySelectorAll('.anchor').forEach((anchor, index) => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = e.target.getAttribute('href');
-        let navIndex = (index + 1) % 5;
-        if (navIndex == 0) {
-          navIndex = 5;
-        }
-
-        gsap.to(window, {
-          scrollTo: {
-            y: (navIndex + 1) * window.innerHeight - window.innerHeight * 0.8,
-          },
-          duration: 1,
-          ease: 'none',
-        });
-      });
-    });
-
-    let maxW = 5000,
-      maxH = 5000;
-
-    if ($('#pc').length) {
-      (gsap3_1 = 140), (maxW = 0);
-      maxH = 5000;
-    } else if ($('#mobile').length) {
-      (gsap3_1 = 85), (maxW = 5000);
-      maxH = 0;
-    }
-
-    var tlWeve = gsap.timeline({
-      scrollTrigger: {
-        // markers: true,
-        trigger: '.section06',
-        pin: true,
-        pinSpacing: true,
-        scrub: true,
-      },
-    });
-    tlWeve
-      .to('.gsap3', { opacity: 1, y: 0, duration: 0.3 })
-      .to('.gsap3-2', { maxWidth: maxW, maxHeight: maxH, duration: 0.6 })
-      .to('.gsap3-1', { height: gsap3_1, duration: 0.2 })
-      .to('.gsap3-1', { opacity: 0, duration: 0.2 })
-      .to('.gsap3-3', { opacity: 1, duration: 0.2 })
-
-      .to('.gsap3', { zIndex: 1, delay: 0.1 });
   },
 };
